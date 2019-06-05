d700210cf369ea5f08d560d5088d1e96c00e921d
unknown <lijiacai@pand-auto.com>
2019-06-05 12:21:54 +0800

    add

---
+++ bin/start.sh
@@ -0,0 +1,30 @@
+
+
+if [ ! -d log ];
+then
+    mkdir log
+fi
+
+# 1.0 start celery worker.
+#nohup python -m celery -A src.task worker --loglevel=info -f ./log/celery.log &
+
+#if [ $? -ne 0 ];
+#then
+#    echo "[ERROR] start celery worker failed."
+#    exit -1
+#else
+#    echo "[INFO] start celery worker ok."
+#fi
+
+# 2.0 start web api
+nohup uwsgi conf/uwsgi.conf  &
+if [ $? -ne 0 ];
+then
+    echo "[ERROR] start uwsgi server failed."
+    exit -1
+else
+    echo "[INFO] start uwsgi server ok."
+fi
+
+echo "server is ready."
+
---
+++ bin/stop.sh
@@ -0,0 +1,13 @@
+uwsgi --stop server.pid
+
+if [ $? -ne 0 ];
+then
+    echo "[ERROR] stop uwsgi server failed."
+    exit -1
+else
+    echo "[INFO] stop uwsgi server ok."
+fi
+
+# ps -ef|grep fresh_msg.py | awk '{print $2}' | xargs kill -9
+
+
